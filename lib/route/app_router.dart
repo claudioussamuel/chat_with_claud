e21e1126087f3d472c4221e2be3de13b4a9b2dad
CLAUDIOUS OPOKU MENSAH <claudiouso00@gmail.com>
2025-05-15 04:19:23 +0000

    (feat) added a auto route

--- lib/route/app_router.dart
+++ lib/route/app_router.dart
@@ -1,1 +1,10 @@
-class AppRouter {}
+import 'package:auto_route/auto_route.dart';
+import 'app_router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Screen,Route')
+class AppRouter extends $AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: MyHomePage.page, initial: true),
+      ];
+}
